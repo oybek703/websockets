3e7670804e9bc456b19c8db70abaa5e2d177bcc2
Yoriqulov Oybek <hhoybek@gmail.com>
2022-12-11 14:39:17 +0500

    basic socket.io server created

---
+++ src/main.ts
@@ -0,0 +1,29 @@
+import 'colors'
+import express from 'express'
+import {createServer} from 'http'
+import {join} from 'path'
+import {Server} from 'socket.io'
+
+const app = express()
+const server = createServer(app)
+const io = new Server(server)
+
+io.on('connection', function (socket) {
+    console.log('user connected')
+    socket.on('new-message', function (message) {
+        io.emit('new-message', message)
+    })
+    socket.on('disconnect', function (reason) {
+        console.group()
+        console.log(reason)
+        console.log('User disconnected!')
+        console.groupEnd()
+    })
+})
+
+app.get('/', (req, res) => {
+    res.sendFile(join(__dirname, 'templates', 'index.html'))
+})
+
+const PORT = 5000
+server.listen(PORT, () => console.log(`Server is running on port ${PORT}...`.blue.underline))
